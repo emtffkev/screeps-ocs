1ffc2b8f5c25107a180f929b19120caa2f8bda70
canisminor1990 <canisminor1990@gmail.com>
2017-04-12 16:04:28 +0800

    update

--- command.js
+++ command.js
@@ -1,2 +1,2 @@
-Game.rooms['W82S67'].setStore('58eca89ba852ca4678cfd6f7', 'energy', 2000);
-Game.rooms['W82S67'].setStore('58eca89ba852ca4678cfd6f7', 'XGH2O', 3000)
+Game.rooms['W82S67'].setStore('58eca89ba852ca4678cfd6f7', RESOURCE_ENERGY, 2000);
+Game.rooms['W82S67'].setStore('58eca89ba852ca4678cfd6f7', RESOURCE_CATALYZED_GHODIUM_ACID, 3000)
